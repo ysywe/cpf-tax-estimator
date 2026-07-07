97dc9d2b63c92f8fd4422f5efb42f23d5278b954
ysywe <thuyaswag21@gmail.com>
2026-07-07 19:54:51 +0800

    feat: setup frontend with React and Vite

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'; 
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), 
+  ],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), 
+      },
+    },
+  },
+});
